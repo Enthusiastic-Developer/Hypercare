bbd0aefdc865de0eb566c6f7e38587da461a5750
NikhilDev <nikhil.devuse404@outlook.com>
2023-12-11 14:12:38 +0530

    Added all the required files for the common

--- BackEnd/Common/ConfigurationService/ConfigSettings.cs
+++ BackEnd/Common/ConfigurationService/ConfigSettings.cs
@@ -45,18 +45,7 @@ namespace ConfigurationService
             string username = AppSettings["username"];
             string pwd = AppSettings["password"];
             string dbname = AppSettings["dbname"];
-            bool IsGsmaEnabled = Convert.ToBoolean(AppSettings["IsGSMAEnabled"]);
             string connString = string.Format("Server={0};Database={1};User ID={2};Password={3};Encrypt={4}", server, dbname, username, pwd, false);
-            if (IsGsmaEnabled)
-            {
-                connString = string.Format("Server={0};Database={1};Trusted_Connection=True;Encrypt={2}", server, dbname, false);
-            }
-
-            string maxPoolSize = AppSettings["MaxPoolSize"];
-            if (!string.IsNullOrWhiteSpace(maxPoolSize))
-            {
-                connString += ";Max Pool Size = " + maxPoolSize;
-            }
             return connString;
         }
 
@@ -66,18 +55,7 @@ namespace ConfigurationService
             string username = AppSettings["username"];
             string pwd = AppSettings["password"];
             string dbname = AppSettings["dbname"];
-            bool IsGsmaEnabled = Convert.ToBoolean(AppSettings["IsGSMAEnabled"]);
             string connString = string.Format("Server={0};Database={1};User ID={2};Password={3};Encrypt={4}", server, dbname, username, pwd, false);
-            if (IsGsmaEnabled)
-            {
-                connString = string.Format("Server={0};Database={1};Trusted_Connection=True;Encrypt={2}", server, dbname, false);
-            }
-
-            string maxPoolSize = AppSettings["MaxPoolSize"];
-            if (!string.IsNullOrWhiteSpace(maxPoolSize))
-            {
-                connString += ";Max Pool Size = " + maxPoolSize;
-            }
             return connString;
         }
     }
