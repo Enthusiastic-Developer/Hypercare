59cceabc95331fd6b32470d39d07cf6d5bfb6900
NikhilDev <nikhil.devuse404@outlook.com>
2023-12-20 12:43:28 +0530

    All the test cases are tested and working for TeamProfiles

--- BackEnd/TeamProfiles/TeamProfilesService.NunitTest/TeamControllerTest.cs
+++ BackEnd/TeamProfiles/TeamProfilesService.NunitTest/TeamControllerTest.cs
@@ -17,7 +17,7 @@
         {
             var teams = await _client.GetAllResponsibleTeam();
             Assert.That(teams, Is.Not.Null, "The returned list of teams should not be null");
-            Assert.That(teams.Count > 0, Is.True, "The returned list of teams should have at least one team");
+            Assert.That(teams, Is.Not.Empty, "The returned list of teams should have at least one team");
             foreach (var team in teams)
             {
                 Assert.Multiple(() =>
@@ -31,6 +31,79 @@
 
                 });
             }
+        }
+
+        [Test]
+        public async Task GetResourceByModuleName_ShouldReturnTeams()
+        {
+            var teams = await _client.GetResourceByModuleName("Account Management");
+            Assert.That(teams, Is.Not.Null, "The returned list of teams should not be null");
+            Assert.That(teams, Is.Not.Empty, "The returned list of teams should have at least one team");
+            foreach (var team in teams)
+            {
+                Assert.Multiple(() =>
+                {
+                    Assert.That(team.NumberOfTasks, Is.GreaterThanOrEqualTo(0), $"NumberOfTasks should be greater than or equal to 0 for team {team.ResponsibleTeam}");
+                    Assert.That(team.OnsiteTeam, Is.Not.Null.Or.Empty.Or.EqualTo(team.PrimaryResource).Or.Null.Or.EqualTo(team.SecondaryResource).Or.Null, $"At least one of OnsiteTeam, PrimaryResource, or SecondaryResource should be present for team {team.ResponsibleTeam}");
+
+                });
+            }
+        }
+
+        [Test]
+        public async Task GetModuleDataByResourceName_ShouldReturnTeams()
+        {
+            var teams = await _client.GetModuleDataByResourceName("Prabhanjan");
+            Assert.That(teams, Is.Not.Null, "The returned list of teams should not be null");
+            Assert.That(teams, Is.Not.Empty, "The returned list of teams should have at least one team");
+            foreach (var team in teams)
+            {
+                Assert.Multiple(() =>
+                {
+                    Assert.That(team.SNo, Is.GreaterThanOrEqualTo(0), $"SNo should be greater than or equal to 0 for team {team.ResponsibleTeam}");
+                    Assert.That(team.Module, Is.Not.Null.Or.Empty, "Module should not be null or empty");
+                    Assert.That(team.ResponsibleTeam, Is.Not.Null.Or.Empty, "ResponsibleTeam should not be null or empty");
+                    Assert.That(team.ActionType, Is.Not.Null.Or.Empty, "ActionType should not be null or empty");
+                    Assert.That(team.NumberOfTasks, Is.GreaterThanOrEqualTo(0), $"NumberOfTasks should be greater than or equal to 0 for team {team.ResponsibleTeam}");
+                    Assert.That(team.OnsiteTeam, Is.Not.Null.Or.Empty.Or.EqualTo(team.PrimaryResource).Or.Null.Or.EqualTo(team.SecondaryResource).Or.Null, $"At least one of OnsiteTeam, PrimaryResource, or SecondaryResource should be present for team {team.ResponsibleTeam}");
+
+                });
+            }
+        }
+
+        [Test]
+        public async Task AddResponsibleTeam_ShouldReturnBool()
+        {
+            var team = new HypercareResponsibleTeam
+            {
+                Module = "Account Management",
+                ResponsibleTeam = "Account Management",
+                ActionType = "Account Management",
+                NumberOfTasks = 0,
+                OnsiteTeam = "Account Management",
+                PrimaryResource = "Account Management",
+                SecondaryResource = "Account Management"
+            };
+            var result = await _client.AddResponsibleTeam(team);
+            Assert.That(result, Is.True, "The returned result should be true");
+        }
+
+        [Test]
+        public async Task UpdateResponsibleTeam_ShouldReturnBool()
+        {
+            var team = new HypercareResponsibleTeam
+            {
+                SNo = 1002,
+                Module = "Account Management",
+                ResponsibleTeam = "Account Management",
+                ActionType = "Account Management",
+                NumberOfTasks = 0,
+                OnsiteTeam = "Account Management",
+                PrimaryResource = "Account Management",
+                SecondaryResource = "Account Management"
+            };
+            var result = await _client.UpdateResponsibleTeam(team);
+            Assert.That(result, Is.True, "The returned result should be true");
         }
     }
 }
--- BackEnd/TeamProfiles/TeamProfilesService.NunitTest/TeamProfileClient.cs
+++ BackEnd/TeamProfiles/TeamProfilesService.NunitTest/TeamProfileClient.cs
@@ -1,26 +1,105 @@
 ﻿
+using System.Net.Http.Json;
+
 namespace TeamProfilesService.NunitTest
 {
     public class TeamProfileClient
     {
-        private readonly string apiBaseUrl;
-        private static readonly ILogger<TeamProfileClient> logger = new NLogLoggerFactory().CreateLogger<TeamProfileClient>();
-        private readonly Microsoft.Extensions.Logging.ILogger _logger = logger;
+        private readonly string _apiBaseUrl;
+        private static readonly ILogger<TeamProfileClient> _logger = new NLogLoggerFactory().CreateLogger<TeamProfileClient>();
 
         public TeamProfileClient(string baseApiUrl)
         {
-            apiBaseUrl = baseApiUrl;
+            _apiBaseUrl = baseApiUrl;
         }
         public async Task<IList<HypercareResponsibleTeam>> GetAllResponsibleTeam()
         {
             _logger.LogInformation("GetAllResponsibleTeam called");
-            string url = $"{apiBaseUrl}/api/Team/GetAllResponsibleTeam";
+            string url = $"{_apiBaseUrl}/api/Team/GetAllResponsibleTeam";
             using HttpClient client = new();
             var response = await client.GetAsync(url);
             if (response.IsSuccessStatusCode)
             {
                 var result = await response.Content.ReadAsStringAsync();
                 return JsonConvert.DeserializeObject<IList<HypercareResponsibleTeam>>(result);
+            }
+            else
+            {
+                _logger.LogError(response.ReasonPhrase);
+                throw new Exception(response.ReasonPhrase);
+            }
+        }
+
+        public async Task<IList<HypercareResponsibleTeam>> GetResourceByModuleName(string moduleName)
+        {
+            _logger.LogInformation("GetResourceByModuleName called");
+
+            // Construct the URL with the moduleName parameter
+            string url = $"{_apiBaseUrl}/api/Team/GetResourceByModueName?moduleName={moduleName}";
+
+            using HttpClient client = new();
+            var response = await client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<IList<HypercareResponsibleTeam>>(result);
+            }
+            else
+            {
+                _logger.LogError(response.ReasonPhrase);
+                throw new Exception(response.ReasonPhrase);
+            }
+        }
+
+        public async Task<IList<HypercareResponsibleTeam>> GetModuleDataByResourceName(string resourceName)
+        {
+            _logger.LogInformation("GetModuleDataByResourceName called");
+            string url = $"{_apiBaseUrl}/api/Team/GetModuleDataByResourceName?resourceName={resourceName}";
+            using HttpClient client = new();
+            var response = await client.GetAsync(url);
+            
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<IList<HypercareResponsibleTeam>>(result);
+            }
+            else
+            {
+                _logger.LogError(response.ReasonPhrase);
+                throw new Exception(response.ReasonPhrase);
+            }
+        }
+
+        public async Task<bool> AddResponsibleTeam(HypercareResponsibleTeam responsibleTeam)
+        {
+            _logger.LogInformation("AddResponsibleTeam called");
+            string url = $"{_apiBaseUrl}/api/Team/AddResponsibleTeam";
+            using HttpClient client = new();
+            var response = await client.PostAsJsonAsync(url, responsibleTeam);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<bool>(result);
+            }
+            else
+            {
+                _logger.LogError(response.ReasonPhrase);
+                throw new Exception(response.ReasonPhrase);
+            }
+        }
+
+        public async Task<bool> UpdateResponsibleTeam(HypercareResponsibleTeam responsibleTeam)
+        {
+            _logger.LogInformation("UpdateResponsibleTeam called");
+            string url = $"{_apiBaseUrl}/api/Team/UpdateResponsibleTeam";
+            using HttpClient client = new();
+            var response = await client.PutAsJsonAsync(url, responsibleTeam);
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<bool>(result);
             }
             else
             {
